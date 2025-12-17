91e7bef76c8d2fb032c0df57230ea0a47ca5716f
Eliud Amukambwa <eliudfromkenya@gmail.com>
2025-12-17 06:01:41 +0300

    Add project files.

---
+++ Pilgrims.NakuruSmartMedia/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pilgrims.NakuruSmartMedia.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
